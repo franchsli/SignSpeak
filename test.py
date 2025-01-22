f23551e499483418748cefb9a1c1343baf656ad9
franchsli <112600427+franchsli@users.noreply.github.com>
2025-01-22 17:07:48 -0500

    Added some type hints

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import os
 from tools import *
 
-gesture_model_path = os.path.abspath("models/gesture_recognizer.task")
-video_folder = os.path.abspath("datasets/10-words-slc-and-3-people/")
+gesture_model_path: str = os.path.abspath("models/gesture_recognizer.task")
+video_folder: str = os.path.abspath("datasets/10-words-slc-and-3-people/")
 
 VideoGesturesRecognizer = VideoHandler(gesture_model_path, video_folder)
 VideoGesturesRecognizer.run()
--- tools.py
+++ tools.py
@@ -30,7 +30,7 @@ class VideoHandler(GestureHandler):
     global_timestamp: int = 0
     
 
-    def handle_gesture(self, video_file, frame_index, result) -> None:
+    def handle_gesture(self, video_file, frame_index: int, result) -> None:
         if result.gestures[0][0].category_name != 'None':
             print(f"Video: {video_file}, Frame {frame_index}: {result.gestures[0][0].category_name}")
         else:
@@ -46,7 +46,7 @@ class VideoHandler(GestureHandler):
             ) as hands:
 
             for video_file in os.listdir(self.video_folder):
-                video_path = os.path.join(self.video_folder, video_file)
+                video_path: str = os.path.join(self.video_folder, video_file)
             
                 if not video_file.endswith((".mp4", ".avi", ".mov")):
                     continue
@@ -58,7 +58,7 @@ class VideoHandler(GestureHandler):
                     print(f"Failed to open video: {video_file}")
                     continue
             
-                frame_index = 0
+                frame_index: int = 0
                 fps = cap.get(cv.CAP_PROP_FPS) or 30  # Default to 30 FPS if unknown
             
                 while True:
