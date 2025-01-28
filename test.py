0be721c9b76af48f573822d1dce7e9a9cb3a4fe9
franchsli <112600427+franchsli@users.noreply.github.com>
2025-01-28 17:46:46 -0500

    Added training models

--- test.py
+++ test.py
@@ -3,21 +3,29 @@ from tools import *
 import tensorflow as tf
 from mediapipe_model_maker import gesture_recognizer
 import matplotlib.pyplot as plt
+import tempfile
 
 gesture_model_path: str = os.path.abspath("models/gesture_recognizer.task")
 video_folder: str = os.path.abspath("datasets/10-words-slc-and-3-people/")
 image_folder: str = os.path.abspath("datasets/Alphabet_SLC/")
 
+# Set a custom temporary directory
+created_models_dir = os.path.abspath("C:/Users/windows/Desktop/created_models")
+os.makedirs(created_models_dir, exist_ok=True)
+tempfile.tempdir = created_models_dir
+print(f"Export directory: {tempfile.gettempdir()}")
+print(f"Is directory writable? {os.access(tempfile.gettempdir(), os.W_OK)}")
+assert os.access(tempfile.gettempdir(), os.W_OK)
 # VideoGesturesRecognizer = VideoHandler(gesture_model_path, video_folder)
 # VideoGesturesRecognizer.run()
 # WebCamGestureRecognizer = WebCamHandler(gesture_model_path)
 # WebCamGestureRecognizer.run()
-print(image_folder)
-labels = []
-for i in os.listdir(image_folder):
-    if os.path.isdir(os.path.join(image_folder, i)):
-        labels.append(i)
-print(labels)
+#print(image_folder)
+#labels = []
+#for i in os.listdir(image_folder):
+#    if os.path.isdir(os.path.join(image_folder, i)):
+#        labels.append(i)
+#print(labels)
 
 # NUM_EXAMPLES = 5
 # 
@@ -32,7 +40,6 @@ print(labels)
 #     fig.suptitle(f"Showing {NUM_EXAMPLES} examples for {label}")
 # 
 # plt.show()
-
 
 data = gesture_recognizer.Dataset.from_folder(
     dirname=image_folder,
@@ -54,5 +61,3 @@ print(f"Test loss:{loss}, Test accuracy:{acc}")
 
 model.export_model()
 
-# requires: from google.colab import files
-#files.download('exported_model/gesture_recognizer.task')
