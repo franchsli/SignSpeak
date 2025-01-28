906a0cb12a09b76f001b84a6b5f174c891934c05
franchsli <112600427+franchsli@users.noreply.github.com>
2025-01-27 19:04:55 -0500

    Added training code

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ import os
 from tools import *
 import tensorflow as tf
 from mediapipe_model_maker import gesture_recognizer
-
 import matplotlib.pyplot as plt
 
 gesture_model_path: str = os.path.abspath("models/gesture_recognizer.task")
@@ -20,16 +19,40 @@ for i in os.listdir(image_folder):
         labels.append(i)
 print(labels)
 
-NUM_EXAMPLES = 5
+# NUM_EXAMPLES = 5
+# 
+# for label in labels:
+#     label_dir = os.path.join(image_folder, label)
+#     example_filenames = os.listdir(label_dir)[:NUM_EXAMPLES]
+#     fig, axs = plt.subplots(1, NUM_EXAMPLES, figsize=(10, 2))
+#     for i in range(NUM_EXAMPLES):
+#         axs[i].imshow(plt.imread(os.path.join(label_dir, example_filenames[i])))
+#         axs[i].get_xaxis().set_visible(False)
+#         axs[i].get_yaxis().set_visible(False)
+#     fig.suptitle(f"Showing {NUM_EXAMPLES} examples for {label}")
+# 
+# plt.show()
 
-for label in labels:
-    label_dir = os.path.join(image_folder, label)
-    example_filenames = os.listdir(label_dir)[:NUM_EXAMPLES]
-    fig, axs = plt.subplots(1, NUM_EXAMPLES, figsize=(10, 2))
-    for i in range(NUM_EXAMPLES):
-        axs[i].imshow(plt.imread(os.path.join(label_dir, example_filenames[i])))
-        axs[i].get_xaxis().set_visible(False)
-        axs[i].get_yaxis().set_visible(False)
-    fig.suptitle(f"Showing {NUM_EXAMPLES} examples for {label}")
 
-plt.show()
+data = gesture_recognizer.Dataset.from_folder(
+    dirname=image_folder,
+    hparams=gesture_recognizer.HandDataPreprocessingParams()
+)
+train_data, rest_data = data.split(0.8)
+validation_data, test_data = rest_data.split(0.5)
+
+hparams = gesture_recognizer.HParams(export_dir=os.path.abspath("created_models"))
+options = gesture_recognizer.GestureRecognizerOptions(hparams=hparams)
+model = gesture_recognizer.GestureRecognizer.create(
+    train_data=train_data,
+    validation_data=validation_data,
+    options=options
+)
+
+loss, acc = model.evaluate(test_data, batch_size=1)
+print(f"Test loss:{loss}, Test accuracy:{acc}")
+
+model.export_model()
+
+# requires: from google.colab import files
+#files.download('exported_model/gesture_recognizer.task')
