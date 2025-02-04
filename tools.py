94944f59ab476c64a9701fe468522819a10a1cdb
franchsli <112600427+franchsli@users.noreply.github.com>
2025-02-04 15:18:36 -0500

    Removed useless VideoHandler code

--- tools.py
+++ tools.py
@@ -6,7 +6,6 @@ from dataclasses import dataclass
 @dataclass
 class GestureHandler:
     model_path: str = None
-    gesture_options = None
 
 
 @dataclass
@@ -14,18 +13,7 @@ class VideoHandler(GestureHandler):
     video_folder: str = None
     global_timestamp: int = 0
 
-    def handle_gesture(self, video_file, frame_index: int, result) -> None:
-        if result.gestures[0][0].category_name != "None":
-            print(
-                f"Video: {video_file}, Frame {frame_index}: {result.gestures[0][0].category_name}"
-            )
-        else:
-            print(
-                f"Video: {video_file}, Frame {frame_index}: Gesture does not belongs to a category."
-            )
-
     def run(self) -> None:
-        self.create_options(self.VisionRunningMode.VIDEO)
 
         for video_file in os.listdir(self.video_folder):
             video_path: str = os.path.join(self.video_folder, video_file)
@@ -39,12 +27,9 @@ class VideoHandler(GestureHandler):
             frame_index: int = 0
             fps = cap.get(cv.CAP_PROP_FPS) or 30  # Default to 30 FPS if unknown
             while True:
-                ret, frame = cap.read()
-                if not ret:
+                success, frame = cap.read()
+                if not success:
                     break
-                rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-                # Ensure global timestamp is used
-                timestamp_ms = int(self.global_timestamp + (frame_index * 1000 / fps))
                 cv.imshow("Video", frame)
                 if cv.waitKey(1) & 0xFF == ord("q"):
                     self.stop()
