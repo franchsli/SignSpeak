e10a81bccceb874013ea04657cf4c9c604964f23
franchsli <112600427+franchsli@users.noreply.github.com>
2025-07-01 17:28:55 -0500

    Add methods to gesture predictor

---
+++ core/predictor.py
@@ -0,0 +1,21 @@
+import numpy as np
+from numpy import ndarray
+from keras.models import load_model, Model
+class GesturePredictor:
+    def __init__(self, model_path, actions: ndarray):
+        """Class to predict gestures
+
+        Args:
+            model_path: Where the keras model is.
+            actions (ndarray): A numpy array containing the model's know
+            words
+        """
+
+        self.model: Model = load_model(model_path)
+        self.actions = actions
+        
+    def predict(self, keypoints):
+        # Convert keypoints list to a numpy array
+        keypoints = np.array(keypoints)
+        # Make a prediction on the keypoints using the loaded model
+        return self.model.predict(keypoints[np.newaxis, :, :])
