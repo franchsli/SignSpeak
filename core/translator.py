668f24b3e0bf95a16499b9627b90fe583fbb1de5
franchsli <112600427+franchsli@users.noreply.github.com>
2025-07-01 18:16:02 -0500

    Add variables to sign language translator

--- core/translator.py
+++ core/translator.py
@@ -3,11 +3,20 @@ from processor import MediaPipeProcessor
 from text_processor import TextProcessor
 
 class SignLanguageTranslator:
-    def __init__(self, config: dict):
+    def __init__(self, model_path: str, actions, mediapipe_confidence: float = 0.75, language = "es"):
+        """The generic class for a sing language translator
+
+        Args:
+            model_path (str): Where the keras model is.
+            actions (ndarray): A numpy array containing the model's known words.
+            mediapipe_confidence (float, optional): The confidence of the mediapipe landmark detection. Defaults to 0.75.
+            language (str, optional): The language's code that will be checked to correct
+            the text. Defaults to "es".
+        """
         
-        self.processor = MediaPipeProcessor(config["confidence"])
-        self.predictor = GesturePredictor(config["model_path"], config["actions"])
-        self.text_processor = TextProcessor(config["language"])
+        self.processor = MediaPipeProcessor(mediapipe_confidence)
+        self.predictor = GesturePredictor(model_path, actions)
+        self.text_processor = TextProcessor(language)
         
     def process_frame(self, frame):
         # Orchestrates the pipeline
