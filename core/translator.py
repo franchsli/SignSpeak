2f75e26972e1ee300788ba14e6bb8c26794e7e77
franchsli <112600427+franchsli@users.noreply.github.com>
2025-07-07 18:19:00 -0500

    Display the translation in the horizontal center

--- core/translator.py
+++ core/translator.py
@@ -140,13 +140,10 @@ class SignLanguageTranslator:
             return sentence if not in_real_time else None
 
     
-    def display_translation(self, frame: ndarray, translation: str):
+    def _get_translation_x_coordinate(self, frame: ndarray, translation: str) -> int:
         # Calculate the size of the text to be displayed and the X coordinate for centering the text on the image
-        textsize = cv.getTextSize(translation, cv.FONT_HERSHEY_SIMPLEX, 1, 2)[0]
-        text_X_coord = (frame.shape[1] - textsize[0]) // 2
-        # Draw the sentence on the frame
-        cv.putText(frame, translation, (text_X_coord, 470),
-                    cv.FONT_HERSHEY_SIMPLEX, 1, (210, 4, 45), 2, cv.LINE_AA)
+        pass
+        
     
     def _display_translation(self, frame: ndarray, translation: str)  -> ndarray:
         # Convert BGR to RGB for PIL
@@ -154,9 +151,11 @@ class SignLanguageTranslator:
         pillow_image = Image.fromarray(rgb_frame)
         # conver the pillow Image to a drawable object
         draw_object = ImageDraw.Draw(pillow_image)
-        text_X_coord = (frame.shape[1] - 40) // 2
-        font = ImageFont.truetype("arial.ttf", 40)
-        draw_object.text((text_X_coord, 470), translation, (210, 4, 45), font)
+        font = ImageFont.truetype("arial.ttf", 35)
+        bbox = draw_object.textbbox((0, 0), translation, font=font)
+        text_width = bbox[2] - bbox[0]
+        text_X_coord = (frame.shape[1] - text_width) // 2
+        draw_object.text((text_X_coord, 470), translation, (86, 24, 201), font, align="center")
         # Convert PIL image (RGB) back to OpenCV image (BGR)
         cv_image = cv.cvtColor(array(pillow_image), cv.COLOR_RGB2BGR)
         # Show the image on the display
