2c7a0c73f34b518637c14992fb382ec735c80e91
Francisco <112600427+franchsli@users.noreply.github.com>
2025-07-19 15:28:39 -0500

    Update the dataset creation code in test.py

--- test.py
+++ test.py
@@ -4,14 +4,10 @@ from tools import VideoHandler
 video_folder = os.path.abspath("data/ADIOS/")
 video_folders_path = os.path.abspath("data")
 video_folders = os.listdir(video_folders_path)
-VideoGesturesRecognizer = VideoHandler(video_folder)
+VideoGesturesRecognizer = VideoHandler(video_folders_path)
 # VideoGesturesRecognizer.create_directories(os.path.abspath("test"))
 # create datasets code
-for video_folder in video_folders:
-    VideoGesturesRecognizer.video_folder = os.path.join(
-        video_folders_path, video_folder
-    )
-    VideoGesturesRecognizer.create_dataset(os.path.abspath("testing"))
+VideoGesturesRecognizer.create_dataset(os.path.abspath("testing"))
 
 # training code (after all the datasets are created)
 # VideoGesturesRecognizer.train(os.path.abspath("testing"))
