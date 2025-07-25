d95bfafe8f7325d47ddd591402689945461cb8d7
Francisco <112600427+franchsli@users.noreply.github.com>
2025-07-25 17:10:35 -0500

    Extend Gesturepredictor to load multiple models

--- core/predictor.py
+++ core/predictor.py
@@ -1,15 +1,19 @@
 from numpy import ndarray
 from keras.models import load_model, Model
 class GesturePredictor:
-    def __init__(self, actions: ndarray, model_path: str = "models/model.keras"):
-        """Class to predict gestures
+    def __init__(self):
+        """Class to predict gestures.
 
         Args:
             actions (ndarray): A numpy array containing the model's known
             words.
             model_path (str, optional): Where the keras model is.
         """
-
-        self.model: Model = load_model(model_path)
-        self.actions = actions
+        self.loaded_models = {}
+        self.loaded_models_actions = {}
+    
+    def load_model(self, actions: list[str], model_name: str, model_path: str = "models/model.keras"):
+        self.loaded_models[model_name] = load_model(model_path)
+        self.loaded_models_actions[model_name] = actions
+        
 
