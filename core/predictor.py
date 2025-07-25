92225056350277deba43fe2271a09b6158d4dfe2
Francisco <112600427+franchsli@users.noreply.github.com>
2025-07-25 18:27:40 -0500

    Remove unnecesary method in geture predictor

--- core/predictor.py
+++ core/predictor.py
@@ -1,4 +1,4 @@
-from numpy import ndarray, newaxis, argmax
+from numpy import ndarray
 from keras.models import load_model, Model
 class GesturePredictor:
     def __init__(self):
@@ -12,19 +12,9 @@ class GesturePredictor:
         self.loaded_models = {}
         self.loaded_models_actions = {}
     
-    def load_model(self, actions: list[str], model_name: str, model_path: str = "models/model.keras"):
+    def load_model(self, actions: ndarray, model_name: str, model_path: str = "models/model.keras"):
         self.loaded_models[model_name] = load_model(model_path)
         self.loaded_models_actions[model_name] = actions
-    
-    def predict(self, keypoints: ndarray, model_name: str):
-        if model_name in self.loaded_models:
-            prediction_model: Model = self.loaded_models[model_name]
-            prediction_model_actions: list[str] = self.loaded_models_actions[model_name]
-            prediction = prediction_model.predict(keypoints[newaxis, :, :])
-            prediction_class = prediction_model_actions[argmax(prediction)]
-            return prediction, prediction_class
-        else:
-            raise ValueError(f"Model {model_name} isn't loaded.")
 
 
 
