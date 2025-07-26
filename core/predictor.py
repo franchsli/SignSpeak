42cddeb2b9ce9136b7cb9dac2bda223667e32cf6
Francisco <112600427+franchsli@users.noreply.github.com>
2025-07-26 15:22:11 -0500

    Add docstring to load_model method

--- core/predictor.py
+++ core/predictor.py
@@ -8,6 +8,13 @@ class GesturePredictor:
         self.loaded_models_actions = {}
     
     def load_model(self, actions: ndarray, model_name: str, model_path: str = "models/model.keras"):
+        """Loads the model in the given path in the class' memory for later use.
+
+        Args:
+            actions (ndarray): An array containing the models known actions (could be words or letters).
+            model_name (str): The name that will be given to the model in memory.
+            model_path (str, optional): Where the model is. Defaults to "models/model.keras".
+        """
         self.loaded_models[model_name] = load_model(model_path)
         self.loaded_models_actions[model_name] = actions
     
