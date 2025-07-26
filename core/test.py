84e2c3a54b05a05a855b57f0f58b85285b9f0e38
Francisco <112600427+franchsli@users.noreply.github.com>
2025-07-26 15:58:39 -0500

    Update test.py function

--- core/test.py
+++ core/test.py
@@ -11,14 +11,19 @@ def test_import_performance():
 
 def test_normal_translation_performance():
     from translator import SignLanguageTranslator
+    from predictor import SignPredictor
     start = time()
     # Set the path to the data directory
     PATH = path.abspath('data')
-    # Create an array of action labels by listing the contents of the data directory
-    actions = array(listdir(PATH))
-    translator = SignLanguageTranslator(actions)
-    x = translator.translate_video(path.abspath("data/ADIOS/ADIOS1.mp4"), False)
-    print(f"Translation {x}")
+    # Create an array of sign labels by listing the contents of the data directory
+    signs = array(listdir(PATH))
+    predictor = SignPredictor()
+    # load the models down here
+    predictor.load_model(signs, "words")
+    # load the predictor instance in the translator
+    translator = SignLanguageTranslator(predictor)
+    translation = translator.translate_video(path.abspath("data/ADIOS/ADIOS1.mp4"), True)
+    print(translation)
     end = time()
     print(f"Proccessing 6 seconds long video took {end - start} seconds")
 
