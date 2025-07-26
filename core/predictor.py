1f7b0b8cac0341ab27912740049ea17e7785a390
Francisco <112600427+franchsli@users.noreply.github.com>
2025-07-26 15:13:46 -0500

    Implement method to return a desired model and its actions

--- core/predictor.py
+++ core/predictor.py
@@ -10,6 +10,13 @@ class GesturePredictor:
     def load_model(self, actions: ndarray, model_name: str, model_path: str = "models/model.keras"):
         self.loaded_models[model_name] = load_model(model_path)
         self.loaded_models_actions[model_name] = actions
+    
+    def get_model(self, model_name: str) -> tuple[Model, str]:
+        if model_name in self.loaded_models:
+            return self.loaded_models[model_name], self.loaded_models_actions[model_name]
+        else:
+            raise ValueError(f"Model '{model_name}' isn't loaded. Load it first and try again.")
+
 
 
 
