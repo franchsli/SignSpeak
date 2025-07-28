83109f625fc603788b31571dd67c2b04dcdeef43
Francisco <112600427+franchsli@users.noreply.github.com>
2025-07-28 15:48:16 -0500

    Format core text_processor.py

--- core/text_processor.py
+++ core/text_processor.py
@@ -1,5 +1,7 @@
 from language_tool_python import LanguageToolPublicAPI
 from language_tool_python.utils import RateLimitError
+
+
 class TextProcessor:
     def __init__(self, language="es"):
         """
@@ -11,15 +13,20 @@ class TextProcessor:
             self.language_tool = LanguageToolPublicAPI(language)
         except RateLimitError:
             self.language_tool = None
-            print("You have exceeded the rate limit for the free LanguageTool API. Please try again later. The class must be re initialized later")
+            print(
+                "You have exceeded the rate limit for the free LanguageTool API. Please try again later. The class must be re initialized later"
+            )
 
-        
     def correct_sentence(self, sentence: str):
         try:
             if self.language_tool:
                 corrected_text = self.language_tool.correct(sentence)
                 return corrected_text
             else:
-                print("You have exceeded the rate limit for the free LanguageTool API. Please try again later.")
+                print(
+                    "You have exceeded the rate limit for the free LanguageTool API. Please try again later."
+                )
         except RateLimitError:
-            print("You have exceeded the rate limit for the free LanguageTool API. Please try again later.")
+            print(
+                "You have exceeded the rate limit for the free LanguageTool API. Please try again later."
+            )
