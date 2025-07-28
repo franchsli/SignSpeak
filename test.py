ee8cd3afbd724bad5a941227844f296ebe339f8b
Francisco <112600427+franchsli@users.noreply.github.com>
2025-07-28 15:58:24 -0500

    Format test.py

--- test.py
+++ test.py
@@ -5,13 +5,12 @@ video_folder = os.path.abspath("data/ADIOS/")
 video_folders_path = os.path.abspath("data")
 video_folders = os.listdir(video_folders_path)
 #VideoGesturesRecognizer = VideoHandler(data_parent_folder=video_folders_path)
-# VideoGesturesRecognizer.create_directories(os.path.abspath("test"))
 # create datasets code
 # VideoGesturesRecognizer.create_dataset(os.path.abspath("testing"))
-
 # training code (after all the datasets are created)
 # VideoGesturesRecognizer.train(os.path.abspath("test"))
 
+# Example with ImageHandler
 ImageSignHandler = ImageHandler(data_parent_folder=os.path.abspath("letters_data"))
 ImageSignHandler.create_dataset(os.path.abspath("letters_test"))
 ImageSignHandler.train(os.path.abspath("letters_test"), "models/letters.keras")
