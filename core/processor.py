6ac73ffd9489521cf604607defce235038d7bc2d
Francisco <112600427+franchsli@users.noreply.github.com>
2025-08-17 16:10:57 -0500

    Update comment in image processing method

--- core/processor.py
+++ core/processor.py
@@ -163,7 +163,8 @@ class MediaPipeProcessor:
         # Process the image using the model
         # MediaPipe works better with read-only images
         image_rgb.flags.writeable = False
-        # TODO: Improve this, model processing, it's so slow that it adds atleast 29 seconds on a 6 seconds long video (and 30 on average)
+        # The model processing is usually the heaviest part, a GPU may be needed for a better experience
+        # when this is used in video_translation
         results = self.model.process(image_rgb)
         image_rgb.flags.writeable = True
 
