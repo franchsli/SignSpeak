634225d8e5072c3960ddb9d7ed18246a93aa4bd2
Francisco <112600427+franchsli@users.noreply.github.com>
2025-11-21 16:30:18 -0500

    test: remove duplicated test

--- core/test.py
+++ core/test.py
@@ -13,26 +13,3 @@ def test_import_performance():
     print(f"Importing SignLanguageTranslator took {end - start} seconds")
 
 
-def test_normal_translation_performance():
-    from translator import SignLanguageTranslator
-    from predictor import SignPredictor
-
-    start = time()
-    # Set the path to the data directory
-    PATH = path.abspath("data")
-    # Create an array of sign labels by listing the contents of the data directory
-    signs = array(listdir(PATH))
-    predictor = SignPredictor()
-    # load the models down here
-    predictor.load_model(signs, "words")
-    # load the predictor instance in the translator
-    translator = SignLanguageTranslator(predictor)
-    translation = translator.translate_video(
-        path.abspath("data/ADIOS/ADIOS1.mp4"), True
-    )
-    print(translation)
-    end = time()
-    print(f"Proccessing 6 seconds long video took {end - start} seconds")
-
-
-test_import_performance()
