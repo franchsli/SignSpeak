bc2b2b42f778e7756d66df3a6a6ae44c4f021f81
Francisco <112600427+franchsli@users.noreply.github.com>
2025-11-22 15:15:18 -0500

    refactor: move SignPredictor methods to SignLanguageTranslator
    This is done because those methods are intended to be used only in SignLanguageTranslator

--- core/translator.py
+++ core/translator.py
@@ -1,8 +1,8 @@
 import cv2 as cv
 from numpy import array, amax, argmax, newaxis, ndarray
+from keras.models import load_model, Model
 from PIL import ImageDraw, ImageFont, Image
 from string import ascii_lowercase, ascii_uppercase
-from predictor import SignPredictor
 from processor import MediaPipeProcessor
 from text_processor import TextProcessor
 
@@ -10,22 +10,21 @@ from text_processor import TextProcessor
 class SignLanguageTranslator:
     def __init__(
         self,
-        predictor: SignPredictor,
         mediapipe_confidence: float = 0.75,
         language="es",
     ):
         """Sign language translation class.
 
         Args:
-            predictor (SignPredictor)
             mediapipe_confidence (float, optional): The minimun detection and tracking confidence
             that the MediaPipe model will have. Defaults to 0.75.
             language (str, optional): The language's code that will be checked to correct
             the text. Defaults to "es".
         """
         self.mediapipe_confidence = mediapipe_confidence
-        self.predictor = predictor
         self.text_processor = TextProcessor(language)
+        self.loaded_models = {}
+        self.loaded_models_signs = {}
 
     def translate_video(
         self,
@@ -374,3 +373,36 @@ class SignLanguageTranslator:
         # Shut off the server
         if self.text_processor.language_tool:
             self.text_processor.language_tool.close()
+    
+    def load_model(
+        self, signs: ndarray, model_name: str, model_path: str = "models/model.keras"
+    ):
+        """Loads the model in the given path in the class' memory for later use.
+
+        Args:
+            signs (ndarray): An array containing the models known signs (could be words or letters).
+            model_name (str): The name that will be given to the model in memory.
+            model_path (str, optional): Where the model is. Defaults to "models/model.keras".
+        """
+        self.loaded_models[model_name] = load_model(model_path)
+        self.loaded_models_signs[model_name] = signs
+
+    def get_model(self, model_name: str) -> tuple[Model, str]:
+        """Returns the model stored in the class with the given name
+        and its signs if found.
+
+        Args:
+            model_name (str): How the model was called when loaded.
+
+        Raises:
+            ValueError: Raised if no model with the given name is found.
+
+        Returns:
+            tuple[Model, str]: The found Model and its signs.
+        """
+        if model_name in self.loaded_models:
+            return self.loaded_models[model_name], self.loaded_models_signs[model_name]
+        else:
+            raise ValueError(
+                f"Model '{model_name}' isn't loaded. Load it first and try again."
+            )
