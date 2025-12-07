40f2f76e4bf1232ffb09d57c93f04a5e3cf8b61b
Francisco <112600427+franchsli@users.noreply.github.com>
2025-12-07 15:54:55 -0500

    test: add GestureHandler get_label_name method unit tests

--- test_tools.py
+++ test_tools.py
@@ -13,3 +13,15 @@ def test_no_file_name():
 def test_file_name_starts_with_index():
     index = test_handler.get_file_index("1ADIOS")
     assert index == "1"
+
+def test_no_alpha_characters():
+    label_name = test_handler.get_label_name("123456789.mp4")
+    assert label_name == ""
+
+def test_middle_dash_character():
+    label_name = test_handler.get_label_name("BUENOS-DIAS.mp4")
+    assert label_name == "BUENOS-DIAS"
+
+def test_starts_with_period():
+    label_name = test_handler.get_label_name(".DIAS.mp4")
+    assert label_name == "DIAS"
