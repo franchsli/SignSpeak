01d62293ca79a50efc8ff473c33ebbd1f2134224
Francisco <112600427+franchsli@users.noreply.github.com>
2025-12-08 17:52:16 -0500

    test: correct test based on previous changes and added tests for correct input values

--- test_tools.py
+++ test_tools.py
@@ -4,25 +4,33 @@ from tools import GestureHandler
 test_handler = GestureHandler()
 
 def test_no_file_index():
-    with raises(ValueError, match="File names must have a defined index."):
+    with raises(ValueError, match=f"File names must have a defined index."):
         test_handler.get_file_index("ADIOS.mp4")
 
 def test_no_file_name():
-    with raises(ValueError, match="File name can't start with a period character."):
+    with raises(ValueError, match="File name must start with an alphabetic character."):
         test_handler.get_file_index(".mp4")
 
 def test_file_name_starts_with_index():
-    index = test_handler.get_file_index("1ADIOS.mp4")
+    with raises(ValueError, match="File name must start with an alphabetic character."):
+        test_handler.get_file_index("1ADIOS.mp4")
+
+def test_index_found():
+    index = test_handler.get_file_index("ADIOS1.mp4")
     assert index == "1"
 
 def test_no_alpha_characters():
-    label_name = test_handler.get_label_name("123456789.mp4")
-    assert label_name == ""
+    with raises(ValueError, match="File name must start with an alphabetic character."):
+        test_handler.get_label_name("123456789.mp4")
+
+def test_label_name_found():
+    label_name = test_handler.get_label_name("ADIOS1.mp4")
+    assert label_name == "ADIOS"
 
 def test_middle_dash_character():
     label_name = test_handler.get_label_name("BUENOS-DIAS.mp4")
     assert label_name == "BUENOS-DIAS"
 
 def test_starts_with_period():
-    with raises(ValueError, match="File name can't start with a period character."):
+    with raises(ValueError, match="File name must start with an alphabetic character."):
         test_handler.get_label_name(".DIAS.mp4")
