ee909e1de81412ffd98c85e66fa5d07168dd4177
Francisco <112600427+franchsli@users.noreply.github.com>
2025-12-16 15:47:29 -0500

    test: remove unnecesary methods used like abspath etc

--- core/test.py
+++ core/test.py
@@ -1,7 +1,7 @@
 """File to test the perfomance of the SignLanguageTranslator"""
 
 from time import time
-from os import path, listdir
+from os import listdir
 from numpy import array
 
 
@@ -14,41 +14,35 @@ def test_import_performance():
 
 def test_video_translation_performance():
     from translator import SignLanguageTranslator
-    # Set the path to the data directory
-    PATH = path.abspath("data")
     # Create an array of sign labels by listing the contents of the data directory
-    signs = array(listdir(PATH))
+    signs = array(listdir("data"))
     translator = SignLanguageTranslator()
     # load the models down here
     translator.load_model(signs, "words")
     start = time()
-    translation = translator.translate_video(path.abspath("data/ADIOS/ADIOS1.mp4"), True)
+    translation = translator.translate_video("data/ADIOS/ADIOS1.mp4")
     end = time()
     print(f"translation: {translation}")
     print(f"Proccessing 6 seconds long video took {end - start} seconds")
 
 def test_image_translation_performance():
     from translator import SignLanguageTranslator
-    # Set the path to the data directory
-    PATH = path.abspath("letters_data")
     # Create an array of sign labels by listing the contents of the data directory
-    signs = array(listdir(PATH))
+    signs = array(listdir("letters_data"))
     translator = SignLanguageTranslator()
     # load the models down here
     translator.load_model(signs, "letters", "models/letterss.keras")
     start = time()
     for _ in range(30):
-        translation = translator.translate_image(path.abspath("letters_data/A/A1.png"))
+        translation = translator.translate_image("letters_data/A/A1.png")
         print(f"translation: {translation}")
     end = time()
     print(f"Time spent translating A1.png 30 times: {end - start} seconds")
 
 def test_live_translation_performance():
     from translator import SignLanguageTranslator
-    # Set the path to the data directory
-    PATH = path.abspath("data")
     # Create an array of sign labels by listing the contents of the data directory
-    signs = array(listdir(PATH))
+    signs = array(listdir("data"))
     translator = SignLanguageTranslator()
     # load the models down here
     translator.load_model(signs, "words")
