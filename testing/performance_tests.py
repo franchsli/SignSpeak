ebcba57c0f6046f5917349fb7b83646b131b8796
franchsli <112600427+franchsli@users.noreply.github.com>
2026-07-07 16:47:20 -0500

    test: rewrite performance tests to use with to close the translator the pythonic way given the previous commit

--- testing/performance_tests.py
+++ testing/performance_tests.py
@@ -15,44 +15,45 @@ def test_import_performance():
 def test_video_translation_performance():
     from core.translator import SignLanguageTranslator
 
-    # Create an array of sign labels by listing the contents of the data directory
-    signs = listdir("data")
-    translator = SignLanguageTranslator()
-    # load the models down here
-    translator.load_model(signs, "words")
-    start = time()
-    translation = translator.translate_video("data/ADIOS/ADIOS1.mp4")
-    end = time()
-    print(f"translation: {translation}")
-    print(f"Proccessing 6 seconds long video took {end - start} seconds")
+    with SignLanguageTranslator() as translator:
+        # Create an array of sign labels by listing the contents of the data directory
+        signs = listdir("data")
+        # load the models down here
+        translator.load_model(signs, "words")
+        start = time()
+        translation = translator.translate_video("data/ADIOS/ADIOS1.mp4")
+        end = time()
+        print(f"translation: {translation}")
+        print(f"Proccessing 6 seconds long video took {end - start} seconds")
 
 
 def test_image_translation_performance():
     from core.translator import SignLanguageTranslator
 
-    # Create an array of sign labels by listing the contents of the data directory
-    signs = listdir("letters_data")
-    translator = SignLanguageTranslator()
-    # load the models down here
-    translator.load_model(signs, "letters", "models/letterss.keras")
-    start = time()
-    for _ in range(30):
-        translation = translator.translate_image("letters_data/A/A1.png")
-        print(f"translation: {translation}")
-    end = time()
-    print(f"Time spent translating A1.png 30 times: {end - start} seconds")
+    with SignLanguageTranslator() as translator:
+        # Create an array of sign labels by listing the contents of the data directory
+        signs = listdir("letters_data")
+        # load the models down here
+        translator.load_model(signs, "letters", "models/letterss.keras")
+        start = time()
+        for _ in range(30):
+            translation = translator.translate_image("letters_data/A/A1.png")
+            print(f"translation: {translation}")
+        end = time()
+        print(f"Time spent translating A1.png 30 times: {end - start} seconds")
 
 
 def test_live_translation_performance():
     from core.translator import SignLanguageTranslator
 
-    # Create an array of sign labels by listing the contents of the data directory
-    signs = listdir("data")
-    translator = SignLanguageTranslator()
-    # load the models down here
-    translator.load_model(signs, "words")
-    start = time()
-    translation = translator.translate_video()
-    end = time()
-    print(f"translation: {translation}")
-    print(f"Live translation took {end - start} seconds")
+    with SignLanguageTranslator() as translator:
+        # Create an array of sign labels by listing the contents of the data directory
+        signs = listdir("data")
+        # load the models down here
+        translator.load_model(signs, "words")
+        start = time()
+        translation = translator.translate_video()
+        end = time()
+        print(f"translation: {translation}")
+        print(f"Live translation took {end - start} seconds")
+
